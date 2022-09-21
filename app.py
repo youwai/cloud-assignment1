c59724414e243cc9146494d1200bee91cf2ddfba
You Wai <lyw5823@gmail.com>
2022-09-21 09:11:02 +0800

    check cursor

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def index():
     cursor = db_conn.cursor()
     create_table = "CREATE TABLE Employees (emp_id varchar(10), name varchar(100), ic_no varchar(50), gender varchar(10), dob Date, age int(2), position varchar(50), department varchar(20), salary double(10,2), created_date Date, primary key (emp_id))"
 
+    print(cursor)
     cursor.execute(create_table)
-    cursor.commit()
     print('Created table')
 
     cursor.execute("Select * from Employees")
