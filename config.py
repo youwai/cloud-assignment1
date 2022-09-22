6d41843ce88599d44ee3282336d1e6e62ab12f11
You Wai <lyw5823@gmail.com>
2022-09-22 16:17:13 +0800

    modified config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = 'hr-db1.cwrevot9vajh.us-east-1.rds.amazonaws.com'
+customhost = 'lab-db.czosgtsbmovs.us-east-1.rds.amazonaws.com'
 customuser = "admin"
 custompass = "hrdb12345"
 customdb = 'HRSystem'
-custombucketname = "jasonsorkeanyung-bucket"
+custombucketname = "leongyouwai-bucket"
 customregionname = "us-east-1"
--- config_backup.py
+++
@@ -1,6 +0,0 @@
-customhost = 'lab-db.czosgtsbmovs.us-east-1.rds.amazonaws.com'
-customuser = "admin"
-custompass = "hrdb12345"
-customdb = 'HRSystem'
-custombucketname = "leongyouwai-bucket"
-customregionname = "us-east-1"
